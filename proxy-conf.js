33182b393a249bff0ce57713fbe49bcdffd5d2c8
SamWang-wrq <113105538+SamWang-wrq@users.noreply.github.com>
2023-01-31 14:22:51 +0800

    Add files via upload

---
+++ proxy-conf.js
@@ -0,0 +1,10 @@
+//代理配置
+let conifg = {
+    '/kdcos/':{
+        target: 'http://127.0.0.1:80',
+    },
+    '/osm/':{
+        target: 'http://120.79.90.199:80',
+    }
+}
+module.exports = conifg
