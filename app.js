5e670d2553ebffa7bd75dea6cb44c7d2e8986498
SamWang-wrq <113105538+SamWang-wrq@users.noreply.github.com>
2023-05-03 21:22:40 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let { Worker, isMainThread } = require('worker_threads');
+//let { Worker, isMainThread } = require('worker_threads');
 let http  = require('http')
 let fs = require('fs')
 let conifg = require('./proxy-conf');
@@ -10,12 +10,10 @@ const querystring=require('querystring');
 //const url=urlread.parse(request.url).query
 //console.log("姓名："+querystring.parse(url).stat);
 
-// 以异步回调方式进行调用 
 
 
 
-
-//new Worker("./show.js");//这样就创建了一个新thread但是好像没必要因为node本身自带强大的异步I/O
+//new Worker("./server.js");=>这样就创建了一个新thread但是好像没必要因为node本身自带强大的异步I/O
 
 
 //前期函数
@@ -74,33 +72,65 @@ function generateIPkey(ipkeys,ip){
 //变量声明
 var ipkeys={};
 var banned={};
+var opts={};
 
 
 //主服务器
 let app=http.createServer(function(request,response){
-	if(typeof(banned[getClientIp(request)])=="undefined"){//判断当前ip是否已经被屏蔽
+
+	if(typeof(banned[getClientIp(request)])=="undefined"){
 		if(request.url!='/favicon.ico'){
-			var this_query=querystring.parse(urlread.parse(request.url).query);//获取当前url中的参数
+			console.log(request);
+			//var this_query=readpara(request);
+			var this_query=querystring.parse(urlread.parse(request.url).query);
 			if(typeof(this_query.stat)=="undefined"){//处理第一次连接的情况
-				var this_key = generateIPkey(ipkeys,getClientIp(request));//生成临时key
-				response.writeHead(302,{"Location":"?stat=0&key="+this_key+"&url="+request.url})//进行重定向
-				response.end()
+				var this_key = generateIPkey(ipkeys,getClientIp(request));
+
+
+				var url=request.url
+				for ( var key in conifg){
+					if( url.indexOf(key) >-1 ){
+						let info = conifg[key].target.split(':')
+						opts[getClientIp(request)]={
+							protocol: info[0]+':',
+							host:    info[1].slice(2),
+							port:    info[2] || 80,
+							method:  request.method,//这里是发送的方法
+							path:    url,    //这里是访问的路径
+							json: true,
+							headers: request.headers
+						}
+					}
+		
+				}
+				
+				response.writeHead(302,{"Location":"?stat=0&key="+this_key+"&headers="+request.headers+"&method="+request.method+"&url="+request.url});
+				//response.end("<script>location.href='http://127.0.0.1:8080/?stat=0&key="+this_key+"&url="+request.url+"'</script>");
+				response.end();
+				
+				console.log("there's a first time");
 			}
-			else if (this_query.stat==0){//处理第二次连接的情况
-				if(this_query.key==ipkeys[getClientIp(request)]){//返回的数字是否是刚才生成的随机key
-					delete ipkeys[getClientIp(request)];//销毁随key
-					allowacess(request,response,this_query);//向服务器获取请求的内容并返回
+			
+			else if (this_query.stat==0){
+				//处理第二次连接：1.判断数字是否正确，2.不正确屏蔽
+				if(this_query.key==ipkeys[getClientIp(request)]){//判断key是否相同
+					console.log("good boy");
+					delete ipkeys[getClientIp(request)];
+					//console.log(request_cach)
+					//allowacess(request_cach[getClientIp(request)],response,this_query);--想
+					allowacess(request,response,this_query,opts[getClientIp(request)]);
+					console.log("delivered-finished");
 				}
 				else{
-					banned[getClientIp(request)]=1;//屏蔽这个ip
-					console.log("sb. got banned........")
+					//屏蔽这个ip
+					banned[getClientIp(request)]=1;
+					console.log("sth bad happened........")
 				}
 			}
 		}
 	}
-	else{//被屏蔽ip的处理
+	else{
 		console.log("sb. that got banned tried to enter.....")
-		response.end("you are banned");
 	}
 })
 app.listen(8080);
@@ -111,24 +141,15 @@ app.listen(8080);
 
 
 //以下为代理转发需要使用的函数：
-function allowacess(request,response,this_query){
-    let url = this_query.url
-	console.log(url)
+function allowacess(request,response,this_query,opt){
+    let url = request.url
+	//console.log(url)
     if(request.url!=='/favicon.ico'){//清除第二次访问
         //请求的数据是否存在代理
         for ( var key in conifg){
             if( url.indexOf(key) >-1 ){
                 let info = conifg[key].target.split(':')
-                let opt = {
-                    protocol: info[0]+':',
-                    host:    info[1].slice(2),
-                    port:    info[2] || 80,//端口无特殊设置默认80
-                    method:  request.method,//这里是发送的方法
-                    path:    url,     //这里是访问的路径
-                    json: true,
-                    headers: request.headers
-                }
-                proxy( opt, response,request )//代理方法
+                proxy( opt, response,request)//代理方法
                 return;
             }
 
@@ -147,11 +168,15 @@ function allowacess(request,response,this_query){
 }
 //代理转发的方法
 function proxy( opt,res ,req){
+	console.log("proxy starting.....");
+	console.log(opt);
+
     var proxyRequest = http.request(opt, function(proxyResponse) { //代理请求获取的数据再返回给本地res
+		console.log("sent the request you required to the server");
         proxyResponse.on('data', function(chunk) {
-            console.log( chunk.toString('utf-8') )//打印真实服务器发送过来的数据-就是控制台里的乱码..
+            //console.log( chunk.toString('utf-8') )//打印真实服务器发送过来的数据-就是控制台里的乱码..
+			console.log("yep there's data all right");
 			res.write(chunk, 'binary');
-			res.write("<h1>hi!!</h1>");//url还原
         });
         //当代理请求不再收到新的数据，告知本地res数据写入完毕。
         proxyResponse.on('end', function() {
@@ -175,6 +200,6 @@ function proxy( opt,res ,req){
     
 }
 
-setInterval(()=>{//每隔1s讲当前屏蔽列表写入磁盘
-	fs.writeFile('./banned_list.json',JSON.stringify(banned),ress=>{console.log('yeee')})
-},1000)
+//setInterval(()=>{
+//	fs.writeFile('./banned_list.json',JSON.stringify(banned),ress=>{console.log('yeee')})
+//},1000)
